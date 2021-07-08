ec223bd8cb015a99713b77c5077f0b1c54cc3127
naimovfayyoz <f.naimov@student.inha.uz>
2021-07-08 20:56:57 +0500

    quote generator

---
+++ quoteGenerator/script.js
@@ -0,0 +1,53 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const newQuoteBtn = document.getElementById("new-quote");
+const twitterBtn = document.getElementById("twitter");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+function newQuote() {
+  const quote = apiQuotes[Math.trunc(Math.random() * apiQuotes.length) + 1];
+  !quote.author
+    ? (authorText.textContent = "Unknown")
+    : (authorText.textContent = quote.author);
+
+  quote.text.length > 120
+    ? quoteText.classList.add("long-quote")
+    : quoteText.classList.remove("long-quote");
+  quoteText.textContent = quote.text;
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  showLoadingSpinner();
+  const apiURL = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    removeLoadingSpinner();
+    newQuote();
+  } catch (error) {}
+}
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+getQuotes();
